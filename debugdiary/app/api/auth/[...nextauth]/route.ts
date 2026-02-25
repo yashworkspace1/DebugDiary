36a60c504519160d9664fa1f0b03a835fe7f86a4
yashworkspace1 <yashsaxena2110@gmail.com>
2026-02-26 01:41:19 +0530

    feat: add secure Google and GitHub OAuth login providers

--- debugdiary/app/api/auth/[...nextauth]/route.ts
+++ debugdiary/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
+import GitHubProvider from 'next-auth/providers/github'
 import { prisma } from '@/lib/prisma'
 
 import { AuthOptions } from 'next-auth'
@@ -12,6 +14,14 @@ export const authOptions: AuthOptions = {
         signIn: '/login',
     },
     providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }),
         CredentialsProvider({
             name: 'credentials',
             credentials: {
